4487bb27b0400326780a72ddf833e7ceb58a49f4
Henry Zhang <yuz087@ucsd.edu>
2020-02-06 13:55:48 -0800

    added navbar and basic html

---
+++ public/js/home.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	console.log("Javascript connected!");
+	// $(".name a").click(listenerFunction);
+}
+
+function listenerFunction(e)
+{
+	// e.preventDefault();
+	// var name = $(this).text();
+	// var newName = anagrammedName(name);
+	// console.log(newName);
+	// $(this).text(newName);
+}
